97275551cd3fc9d56a12b23bad386e7e7865fa93
K3Y4NG <rukeyang@gmail.com>
2018-09-20 08:35:43 -0700

    Add date information

--- crawler.py
+++ crawler.py
@@ -18,11 +18,13 @@ for i in range(6,9): #choose the post number you want
     time.sleep(2)
     title = browser.find_element_by_xpath("//h1[@data-pats='title_text']")
     text = browser.find_element_by_xpath("//div[@id='questionText']")
-    author = browser.find_element_by_xpath("//div[@data-pats='updated_text']")
+    update_text = browser.find_element_by_xpath("//div[@data-pats='updated_text']")
     print("============== post ============")
     print("Title: " + title.text)
     print("No: " + str(i))
-    names = author.find_elements_by_class_name("user_name")
+    post_time = update_text.find_element_by_tag_name("span")
+    names = update_text.find_elements_by_class_name("user_name")
+    print("time: " + post_time.get_attribute("title"))
     if len(names) == 0:
         print("Author: " + "Anonymous")
     else:
@@ -39,6 +41,8 @@ for i in range(6,9): #choose the post number you want
         else:
             for name in names:
                 print("name: " + name.text)
+        post_time = selected_answer.find_element_by_class_name("post_region_actions_meta").find_element_by_tag_name("span")
+        print("time: " + post_time.get_attribute("title"))
         print("text: " + selected_answer.find_element_by_class_name("post_region_text").text)
 
 
@@ -47,13 +51,20 @@ for i in range(6,9): #choose the post number you want
         print("======== discussions =======")
         name = content.find_element_by_class_name("discussion_poster")
         text = content.find_element_by_class_name("actual_text")
+        post_time = content.find_element_by_class_name("dicussion_meta").find_element_by_tag_name("span")
         print("name: " + name.text)
+        print("time: " + post_time.get_attribute("title"))
         print("text: " + text.text)
+
         replies = content.find_elements_by_class_name("existing_reply")
         for reply in replies:
             print("    ======== reply ========")
-            print("    name: " + reply.find_element_by_class_name("user_name").text)
-            print("    text: " + reply.find_element_by_class_name("actual_reply_text").text)
+            name = reply.find_element_by_class_name("user_name")
+            post_time = reply.find_element_by_class_name("dicussion_meta").find_element_by_tag_name("span")
+            text = reply.find_element_by_class_name("actual_reply_text")
+            print("    name: " + name.text)
+            print("    time: " + post_time.get_attribute("title"))
+            print("    text: " + text.text)
 
 
 browser.close()
